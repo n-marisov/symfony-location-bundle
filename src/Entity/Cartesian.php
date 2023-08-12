7d95ab905c6dfd2f7f7b1bc82ddde708a4aaa72b
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-08-12 13:11:43 +0300

    4

--- src/Entity/Cartesian.php
+++ src/Entity/Cartesian.php
@@ -24,4 +24,60 @@ class Cartesian
         $this->z = $z;
     }
 
+    /**
+     * @return float
+     */
+    public function getX(): float
+    {
+        return $this->x;
+    }
+
+    /**
+     * @param float $x
+     * @return $this
+     */
+    public function setX(float $x): self
+    {
+        $this->x = $x;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getY(): float
+    {
+        return $this->y;
+    }
+
+    /**
+     * @param float $y
+     * @return $this
+     */
+    public function setY(float $y): self
+    {
+        $this->y = $y;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getZ(): float
+    {
+        return $this->z;
+    }
+
+    /**
+     * @param float $z
+     * @return $this
+     */
+    public function setZ(float $z): self
+    {
+        $this->z = $z;
+        return $this;
+    }
+
+
+
 }
